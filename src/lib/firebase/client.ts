2f104fb0a92e038f996b6339c1b8e8f469da918e
Sanderson Boff Lyra <sanderboff@gmail.com>
2025-09-19 13:37:26 +0000

    FirebaseError: Firebase: Error (auth/configuration-not-found).     at cr

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBIhvck54Zi3VL3MiL-HAydSMvtuK0R5lM",
+  authDomain: "studio-2752599160-8c510.firebaseapp.com",
+  projectId: "studio-2752599160-8c510",
+  storageBucket: "studio-2752599160-8c510.appspot.com",
+  messagingSenderId: "772728943552",
+  appId: "1:772728943552:web:c3b5db5b21f0ec47f84e25"
 };
 
 

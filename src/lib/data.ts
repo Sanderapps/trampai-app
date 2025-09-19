c88856ac7dcd5748343b76df65f0c79b4932d570
Sanderson Boff Lyra <sanderboff@gmail.com>
2025-09-19 19:11:36 +0000

    mudo de novo o icone mas para um que cobione coma palavra trabalho

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,10 +1,10 @@
 import { Company } from './types';
 import companiesData from './companies.json';
-import { Building2 } from 'lucide-react';
+import { Briefcase } from 'lucide-react';
 
 
 // Attach logos to company data
 export const companies: Company[] = companiesData.map((company, index) => ({
     ...company,
-    logo: Building2,
+    logo: Briefcase,
 }));
